24b69593a613a95a88cd270e52d3e2c1965c82e6
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2018-04-07 15:27:59 +0900

    都市名と現在気温の表示

--- weather/weather/ViewController.swift
+++ weather/weather/ViewController.swift
@@ -23,8 +23,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBOutlet weak var cityNameLabel: UILabel!
     @IBOutlet weak var weatherLabel: UILabel!
     @IBOutlet weak var iconView: UIImageView!
+    @IBOutlet weak var temperatureLabel: UILabel!
     
     // 天気を調べる
     @IBAction func weatherSearch(_ sender: Any) {
@@ -37,8 +39,12 @@ class ViewController: UIViewController {
         Alamofire.request("http://api.openweathermap.org/data/2.5/weather?id=1850147&units=metric&appid=ecea42c72b75c53caefaa93002224dd6").validate().responseJSON {response in
             switch response.result {
             case .success(let value):
-                // 現在の天気を表示
                 let json = JSON(value)
+                // 都市名の表示
+                let cityName = json["name"].stringValue
+                self.cityNameLabel.text = cityName
+                
+                // 現在の天気を表示
                 let name = json["weather"][0]["main"].stringValue
                 self.weatherLabel.text = name
                 
@@ -48,6 +54,11 @@ class ViewController: UIViewController {
                 let imageData = try? Data(contentsOf: url as URL)
                 let image = UIImage(data:imageData!)
                 self.iconView.image = image
+                
+                // 現在気温の表示
+                let temperature = json["main"]["temp"].stringValue
+                print(temperature)
+                self.temperatureLabel.text = "現在の気温：\(temperature)℃"
 
             case .failure(let error):
                 print(error)
